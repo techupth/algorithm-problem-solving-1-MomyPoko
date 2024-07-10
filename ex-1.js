593c74e63df5def6ff7988d8b25e3186ff24207b
Chavanvit <chavanvit.n@gmail.com>
2024-07-10 14:26:46 +0700

    feat: add algo

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,17 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  let keepArray = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        keepArray.push(i, j);
+        return keepArray;
+      }
+    }
+  }
+  return keepArray;
+}
+
+const numbers = [2, 7, 11, 15];
+const target = 9;
+console.log(twoSum(numbers, target));
